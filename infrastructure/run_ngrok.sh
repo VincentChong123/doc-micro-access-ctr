f5396006c55fb1b11fa37d2dd82dd66758009cfa
Vincent Chong <ws.chong.sg@gmail.com>
2026-06-12 19:12:06 +0800

    local ok add .del-env

--- infrastructure/run_ngrok.sh
+++ infrastructure/run_ngrok.sh
@@ -1,1 +1,6 @@
+ps aux | grep ngrok
+echo
+echo
+echo
+
 ngrok http --url=supply-various-paralyze.ngrok-free.dev 3000
--- start_dev.sh
+++ start_dev.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+#  killall node ngrok && pkill -f uvicorn
+
 echo "🚀 Booting up the Ringisho Monorepo..."
 
-# 1. Start Python AI Service (Port 8000)
-echo "🧠 Starting Python AI Service on Port 8000..."
-cd services/ai_service
-# Attempt to activate virtual environment if it exists
-source .venv/bin/activate 2>/dev/null || true
-python main.py &
-AI_PID=$!
+
 
 # 2. Start Node Document Service (Port 4000)
 echo "📄 Starting Node Document Service on Port 4000..."
 cd /home/vin/01-prj/doc-micro-access-ctr/services/document_service
 node server.mjs &
 DOC_PID=$!
+
+
+# # 1. Start Python AI Service (Port 8000)
+# echo "🧠 Starting Python AI Service on Port 8000..."
+# cd services/ai_service
+# # Attempt to activate virtual environment if it exists
+# source .venv/bin/activate 2>/dev/null || true
+# python /home/vin/01-prj/doc-micro-access-ctr/services/ai_service/main.py & AI_PID=$!
+
 
 # Give the backend services 2 seconds to fully boot up so the Gateway health checks pass
 sleep 2
@@ -30,6 +35,7 @@ echo "🌐 Starting Ngrok Tunnel (supply-various-paralyze.ngrok-free.dev) -> Por
 # Using --log=stdout and sending to /dev/null suppresses Ngrok's terminal UI
 ngrok http --domain=supply-various-paralyze.ngrok-free.dev 3000 --log=stdout > /dev/null &
 NGROK_PID=$!
+
 
 # =========================================================
 # THE MAGIC SHUTDOWN TRAP
